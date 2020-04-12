32d43aa07e3980801e9dd6799d57cf08f1f162f3
Wei Ning (DA) <wei.ning@digitalasset.com>
2020-04-12 13:51:40 +1000

    optimization problem

--- opt_matching/README.md
+++ opt_matching/README.md
@@ -4,4 +4,11 @@
 
 source: <https://towardsdatascience.com/how-to-match-two-people-with-python-7583b51ff3f9>
 
-dependencies: pulp
+notebook: <https://github.com/khuyentran1401/linear-programming-with-PuLP/blob/master/Matching%20problem.ipynb>
+
+dependencies: pulp, pandas
+
+it reminds the linear optimization problem in the puppet matching
+system developed at Wt - the core concept is similar:
+given some constraints and variables, can the system reach the
+optimal outcome?
--- opt_matching/opt_matching/teammates.py
+++ opt_matching/opt_matching/teammates.py
@@ -1,5 +1,40 @@
-from pulp import LpProblem, LpMaximize
+from pulp import LpProblem, LpMaximize, LpVariable, lpSum
+import numpy as np
+import pandas as pd
 
 
 def main():
     prob = LpProblem("MatchingEmployees", LpMaximize)
+    employees = range(8)
+    names = [
+        'Ben', 'Kate', 'Thinh', 'Jorge', 'Alfredo', 'Francisco', 'Olivia',
+        'Chris'
+    ]
+    np.random.seed(0)
+    c = np.random.randint(0, 10, (8, 8))
+    np.fill_diagonal(c, 0)
+    match_info = pd.DataFrame(c, index=names, columns=names)
+
+    y = LpVariable.dicts("pair",
+                         [(i, j) for i in employees for j in employees],
+                         cat='Binary')
+    prob += lpSum([(c[i][j] + c[j][i]) * y[(i, j)] for i in employees
+                   for j in employees])
+
+    for i in employees:
+        prob += lpSum(y[(i, j)] for j in employees) <= 1
+        prob += lpSum(y[(j, i)] for j in employees) <= 1
+        prob += lpSum(y[(i, j)]
+                      for j in employees) + lpSum(y[(j, i)]
+                                                  for j in employees) <= 1
+        prob += lpSum(y[(i, j)] for i in employees for j in employees) == 4
+
+    prob.solve()
+    print("Finish matching!\n")
+    for i in employees:
+        for j in employees:
+            if y[(i, j)].varValue == 1:
+                print(
+                    '{} and {} with preference score {} and {}. Total score: {}'
+                    .format(names[i], names[j], c[i, j], c[j, i],
+                            c[i, j] + c[j, i]))
