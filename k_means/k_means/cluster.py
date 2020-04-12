027591dcdf63d1c04b3c88436ae3b041906476c0
Wei Ning (DA) <wei.ning@digitalasset.com>
2020-04-12 10:24:43 +1000

    k means cluster

--- k_means/k_means/cluster.py
+++ k_means/k_means/cluster.py
@@ -11,7 +11,52 @@ def generate_data():
                       cluster_std=0.60,
                       random_state=0)
     plt.scatter(x[:, 0], x[:, 1])
+    # call show() to launch the display window
+    # see: https://pythonspot.com/matplotlib-scatterplot/
+    # plt.show()
+
+    return x, y
 
 
-def main():
-    generate_data()
+def demo_elbow_method(x):
+    # to demo elbow method (the optimal num of clusters is known to be 4)
+    # To get the values used in the graph, we train multiple models using
+    # a different number of clusters and storing the value of the intertia_
+    # property (WCSS) every time.
+    wcss = []
+    for i in range(1, 11):
+        kmeans = KMeans(n_clusters=i,
+                        init='k-means++',
+                        max_iter=300,
+                        n_init=10,
+                        random_state=0)
+        kmeans.fit(x)
+        wcss.append(kmeans.inertia_)
+    plt.plot(range(1, 11), wcss)
+    plt.title('Elbow Method')
+    plt.xlabel('Number of clusters')
+    plt.ylabel('WCSS')
+    plt.show()
+
+
+def k_means(x):
+    # Next, we’ll categorize the data using the optimum number of
+    # clusters (4) we determined in the last step. k-means++ ensures
+    # that you get don’t fall into the random initialization trap.
+    kmeans = KMeans(n_clusters=4,
+                    init='k-means++',
+                    max_iter=300,
+                    n_init=10,
+                    random_state=0)
+    pred_y = kmeans.fit_predict(x)
+    plt.scatter(x[:, 0], x[:, 1])
+    plt.scatter(kmeans.cluster_centers_[:, 0],
+                kmeans.cluster_centers_[:, 1],
+                s=300,
+                c='red')
+    plt.show()
+
+
+if __name__ == '__main__':
+    x, y = generate_data()
+    k_means(x)
